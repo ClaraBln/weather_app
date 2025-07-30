a057d6e3db4744b1692b2fadd5a2c091234550ad
Clara Blanchard <clara-blanchard@live.fr>
2025-07-30 11:04:56 +0200

    Adding the forecast features

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
+from datetime import datetime
 import os
 import requests
 import streamlit as st
+import pandas as pd
 
 
 load_dotenv()  # Load the content of .env into the environnement 
@@ -75,7 +77,42 @@ class WeatherRequest():
         else:
             st.error(f"Unexpected error from weather: {r_weather.status_code}")
             return None
-                
+
+    def get_daily_forecast(self):
+        lat, lon = self.get_coordinate()
+        if lat is None or lon is None:
+            return None
+        try:
+            r_forecast = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units=metric&appid={self.__class__.api_key}', timeout=2)
+        except requests.exceptions.Timeout:
+            st.error("Sorry, the server is taking too long.")
+            return None
+        except Exception as e:
+            st.error(f"Weather API connection error: {e}")
+            return None
+        if r_forecast.status_code == 401:
+            st.error("API key is invalid or unauthorized for forecast.")
+            return None
+        elif r_forecast.status_code == 200:
+            data_forcast = r_forecast.json()['list']
+            forecasts_by_day = {}
+            for entry in data_forcast:
+                dt = datetime.fromtimestamp(entry["dt"])
+                day = dt.date()
+                hour = dt.hour
+                if day not in forecasts_by_day or abs(hour-12) < abs(forecasts_by_day[day]["hour"]-12):
+                    forecasts_by_day[day] = {"entry": entry, "hour": hour}
+            final_forecast = []
+            for days in sorted(forecasts_by_day.keys())[:5]:
+                e = forecasts_by_day[days]["entry"]
+                final_forecast.append({
+                "date": days.strftime("%d/%m"),
+                "min": e["main"]["temp_min"],
+                "max": e["main"]["temp_max"],
+                "desc": e["weather"][0]["description"].capitalize(),
+                "icon": e["weather"][0]["icon"] })
+        return final_forecast
+
 
 class MapRequest:
     api_key = os.getenv('api_map')
@@ -112,13 +149,12 @@ class MapRequest:
         
         
        
-def display_data():
+def display_data_current():
     weather_request = WeatherRequest(city,country)
     temp, feels_temp, humidity, weather_desc, icon_code, min_temp, max_temp = weather_request.get_weather()
     map_request = MapRequest(weather_request)
-    st.write('## Result of the search')
     cols = st.columns(2)
-    cols[0].write(f'**Location:** :blue[{city.capitalize()}, {country.upper()}]')
+    cols[0].write(f'**Location:** :blue[{city.capitalize()}, {country.capitalize()}]')
     cols[0].write(f'**Temperature:** :blue[{temp}°C]')
     cols[0].write(f'**Feels like:** :blue[{feels_temp}°C]')
     cols[0].write(f'**Humidity:** :blue[{humidity}%]')
@@ -129,6 +165,23 @@ def display_data():
     cols[1].image(icon_url, caption=weather_desc.capitalize())
     map_request.get_map()
     
+def display_data_forcast():
+    forecast_request = WeatherRequest(city,country)
+    forecast = forecast_request.get_daily_forecast()
+    if not forecast:
+        st.error("No forecast found.")
+        return
+    st.write(f'**Location:** :blue[{city.capitalize()}, {country.capitalize()}]')
+    header_cols = st.columns([1.5, 2.5, 3])
+    header_cols[0].markdown("**Date**")
+    header_cols[1].markdown("**Weather Description**")
+    header_cols[2].markdown("**Temperatures**")
+    for prevision in forecast:
+        columns = st.columns([1.5, 2.5, 3])
+        columns[0].write(f"**{prevision['date']}**")
+        icon_url = f"https://openweathermap.org/img/wn/{prevision['icon']}@2x.png"
+        columns[1].image(icon_url, width=70, caption=prevision['desc'])
+        columns[2].write(f"Min: :blue[{prevision['min']}°C] / Max: :blue[{prevision['max']}°C]")
     
         
 def main():
@@ -143,7 +196,13 @@ def main():
     st.write(f'You can find the official country code following the ISO 3166 at: https://www.iso.org/obp/ui/#search')
     
     if st.button('Search'):
-        display_data()
+        tab1, tab2 = st.tabs(['Current weather', 'Forcast for 5 days'])
+        with tab1:
+            st.header('Current weather conditions')
+            display_data_current()
+        with tab2:
+            st.header('Forcast for 5 days at 12pm')
+            display_data_forcast()
     
 
 if __name__ == '__main__':
